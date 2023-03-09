9c5a567f97402c8c1d5ee362525d485ce615fe0a
Oleg <oleg.bryuhanov@hotmail.com>
2023-03-09 23:44:04 +0400

    Added server.js

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-const API_PREFIX = '/api/v1';
 
-// Если мы сделаем POST-запрос сюда, то получим верный ответ
-// Если отправим GET-запрос, то получим либо 405 HTTP ошибку, либо 404
-app.get(`${API_PREFIX}/text`, (req, res) => {
-    res.status(200).send("Hello, World!");
-});
-
-app.get(`${API_PREFIX}/json`, (req, res) => {
-    res.setHeader('Content-Type', 'json');
-    res.status(201).send({"data": {"items": [1, 2, 3]}});
+app.use(express.static("dist"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Мой текст и порт: ${PORT}!`);
+    console.log(`Server is running port: ${PORT}!`);
 });
