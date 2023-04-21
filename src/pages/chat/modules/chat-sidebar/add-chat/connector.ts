94a936ad701d436b998debdb3c68264493decce3
Oleg <oleg.bryuhanov@hotmail.com>
2023-04-21 16:27:46 +0400

    lint fixes

--- src/pages/chat/modules/chat-sidebar/add-chat/connector.ts
+++
@@ -1,9 +0,0 @@
-import { TStore } from "@utils";
-import { SearchResult } from "./search-result";
-
-export function connectUsers(store: TStore): { users: SearchResult[] } {
-  const users = store.user.users.map(u => new SearchResult({ user: u }));
-  return {
-    users
-  };
-}
